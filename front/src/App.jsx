de359c97cbec263016d027f9d724be21b788f7e0
Grace0729 <gdinglasan072901@gmail.com>
2024-12-11 15:47:44 +0800

    first commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,27 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
+import { useState } from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 function App() {
-  const router= createBrowserRouter([
+  const router = createBrowserRouter([
     {
-      path:'/',
-      element:<Home/>
+      path: "/",
+      element: <Home />,
     },
     {
-      path:'/login',
-      element:<Login/>
+      path: "/login",
+      element: <Login />,
     },
     {
-      path:'/register',
-      element:<Register/> 
+      path: "/register",
+      element: <Register />,
     },
-  ])
- 
+  ]);
 
   return (
     <Provider store={store}>
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import {Box, Typography} from '@mui/material'
 import { useSelector } from 'react-redux'
 import checkAuth from '../hoc/checkAuth';
+import Calendar from '../component/calendar'
 
 function Home() {
   const user= useSelector(state=> state.auth.user)
 
   return (
-    
     <Box>
         <Typography variant ="h1">Hello, {user?.name ?? "Guest"}</Typography>
+        <Calendar/>
     </Box>
   )
 }
